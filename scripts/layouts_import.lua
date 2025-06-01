5d3d15b910e74f5bf8c766b1fd6d6a089670877d
Legendgreat <syftelurth@gmail.com>
2025-06-01 19:25:02 +0200

    Add files via upload

---
+++ scripts/layouts_import.lua
@@ -0,0 +1,28 @@
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/settings.json")
+Tracker:AddLayouts("layouts/label_grid.json")
+Tracker:AddLayouts("layouts/dungeon_items.json")
+Tracker:AddLayouts("layouts/weather_vane.json")
+
+Tracker:AddLayouts("layouts/tabs.json")
+Tracker:AddLayouts("layouts/dungeons.json")
+Tracker:AddLayouts("layouts/maps/eastern.json")
+Tracker:AddLayouts("layouts/maps/gales.json")
+Tracker:AddLayouts("layouts/maps/hera.json")
+Tracker:AddLayouts("layouts/maps/hyrule_castle.json")
+Tracker:AddLayouts("layouts/maps/dark.json")
+Tracker:AddLayouts("layouts/maps/swamp.json")
+Tracker:AddLayouts("layouts/maps/skull.json")
+Tracker:AddLayouts("layouts/maps/thieves.json")
+Tracker:AddLayouts("layouts/maps/turtle.json")
+Tracker:AddLayouts("layouts/maps/desert.json")
+Tracker:AddLayouts("layouts/maps/ice.json")
+Tracker:AddLayouts("layouts/maps/lorule_castle.json")
+
+Tracker:AddLayouts("layouts/broadcast.json")
+
+--if Tracker.ActiveVariantUID == "standard" then
+Tracker:AddLayouts("layouts/tracker.json")
+--elseif Tracker.ActiveVariantUID == "compact" then
+--    Tracker:AddLayouts("compact/layouts/tracker.json")
+--end
---
+++ scripts/locations_import.lua
@@ -0,0 +1,24 @@
+if Tracker.ActiveVariantUID == "standard" or Tracker.ActiveVariantUID == "compact" then
+  Tracker:AddLocations("locations/hyrule.json")
+  Tracker:AddLocations("locations/lorule.json")
+  Tracker:AddLocations("locations/death_mountain.json")
+
+  Tracker:AddLocations("locations/dungeons/eastern.json")
+  Tracker:AddLocations("locations/dungeons/gales.json")
+  Tracker:AddLocations("locations/dungeons/hera.json")
+  Tracker:AddLocations("locations/dungeons/hyrule_castle.json")
+
+  Tracker:AddLocations("locations/dungeons/dark.json")
+  Tracker:AddLocations("locations/dungeons/swamp.json")
+  Tracker:AddLocations("locations/dungeons/skull.json")
+  Tracker:AddLocations("locations/dungeons/thieves.json")
+  Tracker:AddLocations("locations/dungeons/turtle.json")
+  Tracker:AddLocations("locations/dungeons/desert.json")
+  Tracker:AddLocations("locations/dungeons/ice.json")
+  Tracker:AddLocations("locations/dungeons/lorule_castle.json")
+
+  Tracker:AddLocations("locations/weather_vanes.json")
+  Tracker:AddLocations("locations/cracks_hyrule.json")
+  Tracker:AddLocations("locations/cracks_lorule.json")
+  Tracker:AddLocations("locations/maiamai.json")
+end
